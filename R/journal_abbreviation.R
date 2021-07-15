63953994eba58418dc257f1faac923f298991272
Neal Haddaway <nealhaddaway@Neals-iMac.local>
2021-07-15 19:27:01 +0200

    Update journal_abbreviation()

--- R/journal_abbreviation.R
+++ R/journal_abbreviation.R
@@ -3,17 +3,22 @@
 #' Support matching where journal cannot be found but there is an almost full match with abbreviated or unabbreviated journal name
 #' @param data A dataframe
 #' @param journal_names column in the dataframe that contains the titles of the sources
-
+#' @examples 
+#' \dontrun{
+#' bib_data <- synthesisr::read_refs('inst/extdata/export.ris')
+#' bib_data_new <- journal_abbreviation(bib_data, bib_data$journal)
+#' }
+#' 
 journal_abbreviation <- function(data, journal_names) {
   journal_names <- dplyr::enquo(journal_names)
   ref_list <- bibfix::wos_abbrev_table
   ref_list <- ref_list %>%
-    mutate(source = str_to_sentence(full))
+    dplyr::mutate(source = stringr::str_to_sentence(full))
   data <- data %>%
-    mutate(source = str_to_sentence(!!journal_names))
+    dplyr::mutate(source = stringr::str_to_sentence(!!journal_names))
 
   data <- data %>%
-    inner_join(., ref_list)
+    dplyr::inner_join(., ref_list)
   return(data)
   
 ## need to add in a fuzzy matching function to output a list of potential matches if there are NAs
