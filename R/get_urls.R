d08becfc4381d616e9f9a05197b728f202c5fb8f
Neal Haddaway <nealhaddaway@Neals-iMac.local>
2021-07-17 09:20:01 +0200

    get_urls() now works for searching lens.org

--- R/get_urls.R
+++ R/get_urls.R
@@ -5,6 +5,17 @@
 #' @param bib_data A bibliographic data set stored as a dataframe, with 
 #' each record as a row. Records must contains a column called 'doi' that 
 #' contains the DOIs for the records.
+#' @param source Source to use for obtaining information. Options are 
+#' Unpaywall ('unpaywall', searched via the roadoi package) or The 
+#' Lens.org ('lens'). The former is freeand requires only registration 
+#' of an email address within the R environment. The Lens.org requires 
+#' application for a free, time-limited API token. The ShinyApp includes 
+#' token-free access. Other users must request a Lens.org token here: 
+#' https://www.lens.org/lens/user/subscriptions#scholar.
+#' @param combine Logical argument to set whether the url outputs 
+#' should be combined into a single field (needed for exporting as an 
+#' RIS), or whether they should be retained as seperate columns in the 
+#' dataset. Default is to combine (combine = TRUE).
 #' @return A revised dataframe containing various urls for records that 
 #' could be found using the API.
 #' @importFrom roadoi oadoi_fetch
@@ -12,8 +23,11 @@
 #' @examples 
 #' \dontrun{
 #' bib_data<-revtools::read_bibliography('inst/extdata/export.ris')
+#' get_urls(bib_data)
 #' }
-get_urls <- function(bib_data){
+get_urls <- function(bib_data,
+                     source = 'lens',
+                     combine = TRUE){
   
   #if bibliographic data lacks url field, add an empty one
   if (is.element('url', colnames(bib_data)) == FALSE){
@@ -26,52 +40,123 @@ get_urls <- function(bib_data){
   #subset that have a doi
   url_query <- subset(missing_url, is.na(doi) == FALSE)
   
-  #reset results
-  results <- data.frame(doi = NULL, abstract = NULL)
-  
-  # Source 1: Unpaywall via roadoi ----
-  #start timer
-  t0 <- Sys.time()
-  for (a in 1:nrow(url_query)){
-    #send the doi of the record as a query to CrossRef and get results back
-    response <- tryCatch(
-      suppressWarnings(roadoi::oadoi_fetch(dois = url_query$doi[a],
-                          email = 'neal.haddaway@sei.org')),
-      error=function(e) {return(NA)})
+  # Get results from Unpaywall via roadoi package----
+  #not sure if this is working... 
+  if (identical(source, 'unpaywall') == TRUE){
+    #reset results
+    results <- data.frame(doi = NULL, url = NULL)
     
-    url_best <- do.call(rbind.data.frame, response[[2]])$url
-    if(is.null(url_best) == TRUE){
-      url_best <- NA
+    #start timer
+    t0 <- Sys.time()
+    for (a in 1:nrow(url_query)){
+      #send the doi of the record as a query to CrossRef and get results back
+      response <- tryCatch(
+        suppressWarnings(roadoi::oadoi_fetch(dois = url_query$doi[a],
+                                             email = 'neal.haddaway@sei.org')),
+        error=function(e) {return(NA)})
+      
+      url_best <- do.call(rbind.data.frame, response[[2]])$url
+      if(is.null(url_best) == TRUE){
+        url_best <- NA
+      }
+      url_pdf <- do.call(rbind.data.frame, response[[2]])$url_for_pdf
+      if(is.null(url_pdf) == TRUE){
+        url_pdf <- NA
+      }
+      oa_status <- response$oa_status
+      
+      #build dataframe
+      data <- data.frame(doi = url_query$doi[a],
+                         url = url_best,
+                         pdf = url_pdf,
+                         oa_status = oa_status)
+      results <- rbind(results, data)
     }
-    url_pdf <- do.call(rbind.data.frame, response[[2]])$url_for_pdf
-    if(is.null(url_pdf) == TRUE){
-      url_pdf <- NA
-    }
-    oa_status <- response$oa_status
+    #stop timer
+    t1 <- Sys.time()
+    #elapsed time
+    time <- t1 - t0
     
-    #build dataframe
-    data <- data.frame(doi = url_query$doi[a],
-                       url = url_best,
-                       pdf = url_pdf,
-                       oa_status = oa_status)
-    results <- rbind(results, data)
+    #join and tidy up the results
+    returns <- nrow(subset(results, is.na(url) == FALSE))
+    results <- merge(x = bib_data, y = results, by = "doi", all.x = TRUE)
+    results <- subset(results, select = -c(abstract.x))
+    results <- dplyr::rename(results, abstract = abstract.y)
+    
+    report <- paste0('Your bibliographic dataset contained ',
+                     nrow(missing_doi),
+                     ' records without digital object identifiers.')
   }
-  #stop timer
-  t1 <- Sys.time()
-  #elapsed time
-  time <- t1 - t0
-  
-  #join and tidy up the results
-  returns <- nrow(subset(results, is.na(url) == FALSE))
-  results <- merge(x = bib_data, y = results, by = "doi", all.x = TRUE)
-  results <- subset(results, select = -c(abstract.x))
-  results <- dplyr::rename(results, abstract = abstract.y)
   
   
-  
-  report <- paste0('Your bibliographic dataset contained ',
-                   nrow(missing_doi),
-                   ' records without digital object identifiers.')
+  # Get results from lens.org----
+  if (identical(source, 'lens') == TRUE){
+
+    request <- paste0('{
+	"query": {
+		"terms": {
+			"doi": [', paste0('"', paste(url_query$doi, collapse = '", "'), '"'), ']
+		}
+	},
+	"size":500,
+	"scroll": "1m",
+	"include": ["external_ids", "source_urls"]
+}')
+    #"lens_id", "authors", "publication_type", "title", "external_ids", "start_page", "end_page", "volume", "issue", "references", "scholarly_citations", "source_urls", "abstract", "date_published", "year_published", "references_count", "scholarly_citations_count", "source"
+    
+    # perform search and extract results - this requires an API token
+    data <- getLENSData(token, request)
+    
+    # report requests remaining within the limit (currently 50/min)
+    requests_remaining <- data[["headers"]][["x-rate-limit-remaining-request-per-minute"]]
+    print(paste0('Remaining requests = ', requests_remaining))
+    
+    # extract the JSON content of the response
+    record_json <- httr::content(data, "text")
+    
+    # convert json output from article search to list
+    record_list <- jsonlite::fromJSON(record_json) 
+    results <- as.data.frame(record_list)
+    
+    #extract dois
+    results$doi <- unlist(lapply(results$data.external_ids, function(ch) expss::vlookup('doi', ch, result_column = 'value', lookup_column = 'type')))
+    results$url_html <- unlist(lapply(results$data.source_urls, function(ch) {
+      if (is.null(expss::vlookup('html', ch, result_column = 'url', lookup_column = 'type')[1]) == TRUE){
+        return(NA)
+      } else {expss::vlookup('html', ch, result_column = 'url', lookup_column = 'type')[1]}
+    }))
+    results$url_pdf <- unlist(lapply(results$data.source_urls, function(ch) {
+      if (is.null(expss::vlookup('pdf', ch, result_column = 'url', lookup_column = 'type')[1]) == TRUE){
+        return(NA)
+      } else {expss::vlookup('pdf', ch, result_column = 'url', lookup_column = 'type')[1]}
+    }))
+    results <- data.frame(url_html = results$url_html, url_pdf = results$url_pdf, doi = results$doi)
+    
+    #join and tidy up the results
+    returns_html <- nrow(subset(results, is.na(results$url_html) == FALSE))
+    returns_pdf <- nrow(subset(results, is.na(results$url_pdf) == FALSE))
+    results <- merge(x = bib_data, y = results, by.x = 'doi', by.y = 'doi', all.x = TRUE)
+    
+    #if combine = TRUE, combine fields
+    new_url <- paste(results$url, results$url_html, results$url_pdf, sep = ' ')
+    new_url <- stringr::str_replace_all(new_url, "NA", "")
+    new_url <- gsub("\\s+", " ", stringr::str_trim(new_url))
+    results$url <- stringr::str_replace_all(new_url, " ", "; ")
+    results <- subset(results, select = -c(url_html, url_pdf))
+    
+    #compile report and print to console
+    report <- paste0('Your bibliographic dataset contained ',
+                     nrow(missing_url),
+                     ' records without URLs, ',
+                     nrow(url_query),
+                     ' of which had DOIs that could be used to search for URLs on Lens.org. ',
+                     returns_html,
+                     ' of these records had URLs corresponding to web locations and ',
+                     returns_pdf,
+                     ' had URLs corresponding to PDF versions on Lens.org.',
+                     if(combine == TRUE){' The resulting URLs have been condensed into a single field for RIS export.'})
+    
+  }
 
   print(report)
   return()
